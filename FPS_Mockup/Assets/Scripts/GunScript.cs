ae30f2d4d7a5faa5a2e1dc43fc20096f0ef920f6
aleckzander <ghosttchasee@gmail.com>
2023-12-12 18:33:58 -0500

    added raycast shooting

---
+++ FPS_Mockup/Assets/Scripts/GunScript.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+public class GunScript : MonoBehaviour
+{
+    public float damage = 10f;
+    public float range = 100f;
+    public float fireRate = 15f;
+    public float impactForce = 30f;
+
+    public Camera fpsCam;
+    public ParticleSystem muzzleFlash;
+    public GameObject impactEffect;
+
+    private float nextTimeToFire = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1") && Time.time >= nextTimeToFire)
+        {
+            nextTimeToFire = Time.time + 1f / fireRate;
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        muzzleFlash.Play();
+        RaycastHit hit;
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+
+            Enemy enemy  = hit.transform.GetComponent<Enemy>();
+            if ( enemy != null)
+            {
+                enemy.TakeDamage(damage);
+            }
+
+            if (hit.rigidbody != null)
+            {
+                hit.rigidbody.AddForce(-hit.normal * impactForce);
+            }
+
+            GameObject impactGO = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+            Destroy(impactGO, 1f);
+        }
+    }
+}
