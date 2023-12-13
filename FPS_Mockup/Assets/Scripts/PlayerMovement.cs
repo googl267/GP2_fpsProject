237b6051fc68f2cfebdaf4644cde16823950c78a
Vincent Puppo <24-puppovin@soudertonsd.us>
2023-12-13 14:29:07 -0500

    added dynamic fov

--- FPS_Mockup/Assets/Scripts/PlayerMovement.cs
+++ FPS_Mockup/Assets/Scripts/PlayerMovement.cs
@@ -19,18 +19,25 @@ public class PlayerMovement : MonoBehaviour
     Vector3 velocity;
     bool isGrounded;
 
-    // Start is called before the first frame update
+    public float fovCurrent;
+    public float fovDefault;
+    private float fovSprint;
+
     void Start()
     {
-        
+        //Start the Camera field of view at 60
+        fovDefault = Camera.main.fieldOfView;
+        fovSprint = fovDefault * 1.15f;
     }
+
 
     // Update is called once per frame
     void Update()
     {
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        fovCurrent = Camera.main.fieldOfView;
 
-        if(isGrounded && velocity.y < 0)
+        if (isGrounded && velocity.y < 0)
         {
             velocity.y = -2f;
         }
@@ -40,24 +47,53 @@ public class PlayerMovement : MonoBehaviour
 
         Vector3 move = transform.right * x + transform.forward * z;
 
+
+        // allow the player to sprint with shift  !! change to use built in sprint button !!
         if(Input.GetKey(KeyCode.LeftShift))
         {
-            speed = walk * sprint;
+            speed = sprint;
+            FovSmoothSwitcher(fovSprint, 0.095f);
         } 
         else 
         {
             speed = walk;
+            FovSmoothSwitcher(fovDefault, 0.1f);
         }
 
+        // moving the player
         controller.Move(move * speed * Time.deltaTime); 
 
+        // allow the player to jump only if grounded
         if(Input.GetButtonDown("Jump") && isGrounded)
         {
             velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
         }
 
+        // Gravity
         velocity.y += gravity * Time.deltaTime;
-
         controller.Move(velocity * Time.deltaTime);
+
+        Debug.Log(Camera.main.fieldOfView);
+
+    }
+
+    // Smooth FOV change (target_fov, intergral amount)
+    void FovSmoothSwitcher(float fov, float fovSmooth)
+    {
+        if (Camera.main.fieldOfView >= (fov - fovSmooth) && Camera.main.fieldOfView <= (fov + fovSmooth))
+        {
+            Camera.main.fieldOfView = fov;
+            return;
+        }
+
+        if (Camera.main.fieldOfView >= fov)
+        {
+            Camera.main.fieldOfView = Camera.main.fieldOfView - fovSmooth;
+        } else if (Camera.main.fieldOfView <= fov)
+        {
+            Camera.main.fieldOfView = Camera.main.fieldOfView + fovSmooth;
+        }
+
+        return;
     }
 }
